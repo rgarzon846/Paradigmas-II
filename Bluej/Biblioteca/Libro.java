300a89c9f2602b697f5377a9ecdb3393ebb60fd1
rgarzon846 <rgarzon846@alumnos.iua.edu.ar>
2026-03-20 08:38:00 -0300

    vault backup: 2026-03-20 08:38:00

---
+++ Bluej/Biblioteca/Libro.java
@@ -0,0 +1,39 @@
+
+/**
+ * Write a description of class Libro here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Libro
+{
+    // instance variables - replace the example below with your own
+    private 
+    String titulo;
+    String autor;
+    String estado;
+
+    /**
+     * Constructor for objects of class Libro
+     */
+    public 
+    Libro()
+    {
+        // initialise instance variables
+        titulo = "";
+        autor = "";
+        estado = "";
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
