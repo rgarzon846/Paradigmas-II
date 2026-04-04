cdd363ae45a665858f43d0006f78c3855e6b7172
rgarzon846 <rgarzon846@alumnos.iua.edu.ar>
2026-04-04 17:04:54 -0300

    vault backup: 2026-04-04 17:04:54

---
+++ Bluej/MaquinaExpendedora/Destino.java
@@ -0,0 +1,40 @@
+
+/**
+ * Write a description of class Destino here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Destino
+{
+    // instance variables - replace the example below with your own
+    private 
+    String lugar;
+    int valor;
+
+    /**
+     * Constructor for objects of class Destino
+     */
+    public Destino()
+    {
+        // initialise instance variables
+        lugar = "Punta Cana";
+        valor = 9600;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public 
+    int getValor()
+    {
+        // put your code here
+        return valor;
+    }
+    String getLugar(){
+        return lugar;
+    }
+}
---
+++ Bluej/MaquinaExpendedora/Maquina.java
@@ -0,0 +1,40 @@
+
+/**
+ * Write a description of class Maquina here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+
+public class Maquina
+{
+    // instance variables - replace the example below with your own
+    private 
+    int dinero_ingresado;
+    Destino destino;
+    Scanner sc;
+    /**
+     * Constructor for objects of class Maquina
+     */
+    public Maquina()
+    {
+        // initialise instance variables
+        dinero_ingresado = 0;
+        destino = new Destino();
+        sc = new Scanner(System.in);
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public 
+    void pagarViaje()
+    {
+        // put your code here
+        System;
+    }
+}
