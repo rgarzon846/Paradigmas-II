491f0ce140fefffbd0b99621488a944fe0891edb
rgarzon846 <rgarzon846@alumnos.iua.edu.ar>
2026-04-04 17:34:55 -0300

    vault backup: 2026-04-04 17:34:55

--- Bluej/MaquinaExpendedora/Maquina.java
+++ Bluej/MaquinaExpendedora/Maquina.java
@@ -36,11 +36,13 @@ public class Maquina
     {
         // put your code here
         System.out.println("De que monto es el billete con el que desea pagar?:");
-        System.out.println("a. 50\nb. 100\nc. 500\nd. 1000");
+        System.out.println("50\n100\n500\n1000");
         dinero_ingresado = sc.nextInt();
         System.out.println("Hasta el momento se han ingresado: " + llevarCuenta(dinero_ingresado));
         if(habilitacion(llevarCuenta(dinero_ingresado))){
-            
+            System.out.println("Ya puede emitir su ticket!");
+        }else{
+            System.out.println("Faltan ingresar: " + (destino.getValor() - llevarCuenta(dinero_ingresado)));
         }
     }
     private
@@ -49,6 +51,7 @@ public class Maquina
         suma += dinero_ingresado;
         return suma;
     }
+    private
     boolean habilitacion(int suma){
         if(suma > destino.getValor()){
             return true;
@@ -56,5 +59,16 @@ public class Maquina
             return false;
         }
     }
-    
+    public
+    void emitirTicket(){
+        if(habilitacion(llevarCuenta(dinero_ingresado))){
+            System.out.println("###############");
+            System.out.println("Trencitos Argentinos");
+            System.out.println("Ticket precio: " + destino.getValor());
+            System.out.println("Destino: " + destino.getLugar());
+            System.out.println("###############");
+        }else{
+            System.out.println("Complete el pago para obtener su ticket!");
+        }
+    }
 }
