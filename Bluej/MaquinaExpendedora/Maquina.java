c3f58fd02e372842730760fb43dd8533ddcaa06a
rgarzon846 <rgarzon846@alumnos.iua.edu.ar>
2026-04-04 18:14:57 -0300

    vault backup: 2026-04-04 18:14:57

--- Bluej/MaquinaExpendedora/Maquina.java
+++ Bluej/MaquinaExpendedora/Maquina.java
@@ -42,8 +42,12 @@ public class Maquina
         System.out.println("De que monto es el billete con el que desea pagar?:");
         System.out.println("50\n100\n500\n1000");
         dinero_ingresado = sc.nextInt();
+        if(comprobarValor(dinero_ingresado)){
         System.out.println("Hasta el momento se han ingresado: " + llevarCuenta(dinero_ingresado));
         System.out.println("Faltan ingresar: " + (destino.getValor() - suma));
+        }else{
+            System.out.println("No se ingreso un billete valido");
+        }
         }while(!habilitacion());
             System.out.println("Ya puede emitir su ticket!");
     }
@@ -55,6 +59,14 @@ public class Maquina
     private
     boolean habilitacion(){
         if(suma >= destino.getValor()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    private
+    boolean comprobarValor(int billete){
+        if(billete == 100 || billete == 500 || billete == 50 || billete == 1000){
             return true;
         }else{
             return false;
