35ff7d160ea204d3270bbeb6a7e99db1a2acfd23
rgarzon846 <rgarzon846@alumnos.iua.edu.ar>
2026-04-09 20:23:53 -0300

    vault backup: 2026-04-09 20:23:53

---
+++ Bluej/ChatBotsin/Chat.java
@@ -0,0 +1,39 @@
+
+/**
+ * Write a description of class Chat here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Chat
+{
+    // instance variables - replace the example below with your own
+    private 
+    Bot bot;
+    Usuario usuario;
+    String palabra_cierre;
+
+    /**
+     * Constructor for objects of class Chat
+     */
+    public Chat()
+    {
+        // initialise instance variables
+        bot = new Bot();
+        usuario = new Usuario();
+        palabra_cierre = "fin";
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public 
+    void Conversacion()
+    {
+        // put your code here
+        return x + y;
+    }
+}
